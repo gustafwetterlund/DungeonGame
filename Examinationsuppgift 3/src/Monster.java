8f301838f94aa35f9ac620c736db5839b244e76d
gustafwetterlund <58036889+gustafwetterlund@users.noreply.github.com>
2021-01-05 14:25:04 +0100

    Update Monster.java

--- Examinationsuppgift 3/src/Monster.java
+++ Examinationsuppgift 3/src/Monster.java
@@ -1,50 +1,59 @@
 import java.util.Random;
 
-public class Monster {
- Random randMonst = new Random();
+public class Monster 
+{
+ // String som slumpmässigt hämtar ett element från en array med diverse monsternamn med hjälp av en random.
+ Random randMonst = new Random(); 
  private String[] randomMonster = {"Troll","Zombie","Vampire","Skeleton","Orc","Dark Wizard","Frost Giant","Undead Knight"};
  private String rndMonster = randomMonster[randMonst.nextInt(randomMonster.length)];
+ 
+ //Deklaration av privata primitiva variabler för Setters & Getters.
  private String monsterNamn;
  private int monsterSkada;
  private int monsterHp;
  
+ // Konstruktor för Monster.
  public Monster(String monsterName,int monsterDamage,int monsterHealth){
  this.monsterNamn = monsterName;
  this.monsterSkada = monsterDamage;
  this.monsterHp = monsterHealth;
-         
+   
+ // Getters & Setters för att hämta/ändra privata primitiva variabler utanför den lokala klassen.
  }
-    
+ // Getter för att hämta MonsterHp 
 public int getMonsterHp(){
 return this.monsterHp;
+// Setter för att bestämma MonsterHp
 }
 public void setMonsterHp(int monsterHp){
 this.monsterHp = monsterHp;
 }
-
+// Getter för att hämta MonsterNamn
 public String getMonsterName(){
 return this.monsterNamn;
 }
-public void setMonsterName(String monsterNamnTest){
+// Setter för att bestämma MonsterNamn
+ public void setMonsterName(String monsterNamnTest){
  this.monsterNamn = monsterNamnTest;
 }
+ // Metod för Combat, där monstret tar den skada som spelaren har definerad.
 public void monsterTakingDamage(){
 this.monsterHp -= Player.getDamage();
 
-}
+} // Ta bort denna.
 public void monsterTakeSwordDmg(){
 this.monsterHp -= 20;
 }
 
-
+// Getter för att hämta monsterSkada
 public int getMonsterDamage(){
 return this.monsterSkada;
 }
-
+// Setter för att bestämma monsterSkada
 public void setMonsterDamage(int monsterSkada){
  this.monsterSkada = monsterSkada;
 }
-
+//Metod som använder sig utav vår rndMonster-String och skapar ett slumpmässigt namn åt monsterName i konstruktorn när man kallar efter den.
 public String monsterRandomizer(){
 this.monsterNamn = rndMonster;
 return this.monsterNamn;
